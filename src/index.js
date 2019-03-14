d5fc349c157c5c8d7fc625333bcf9cb7b88b90c2
Jan Hamara <jancimac@jans-mbp.lan>
2019-03-14 15:25:35 +0000

    complete boilerplate with correct webpack config

--- src/index.js
+++ src/index.js
@@ -8,14 +8,3 @@ import './index.less';
 // AOS Framework
 import '../node_modules/aos/dist/aos.css';
 import '../node_modules/aos/dist/aos.js';
-
-function component() {
-    let element = document.createElement('div');
-
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
