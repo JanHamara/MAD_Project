629b3dfc98b5fd434370f94e2318f41504579aa0
Jan Hamara <jancimac@jans-mbp.lan>
2019-03-14 15:28:10 +0000

    new boilerplate updated

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// index.js
+
+import _ from 'lodash';
+
+// Main Stylesheet
+import './index.less';
+
+// AOS Framework
+import '../node_modules/aos/dist/aos.css';
+import '../node_modules/aos/dist/aos.js';
