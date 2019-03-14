a0ceef2739446dfc993e533582ee70b1e698f52b
Jan Hamara <jancimac@jans-mbp.lan>
2019-03-14 14:34:28 +0000

    webpack noconfig setup

--- js/index.js
+++
@@ -1,6 +0,0 @@
-// Webpack
-
-// require('../css/vendor/aos.css');
-// require('../css/vendor/bootstrap-theme.min.css');
-// require('../css/vendor/bootstrap.min.css');
-// require('../css/index.css');
