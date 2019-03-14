aa8cc087340d000de37d75ad67142865c60cad22
Jan Hamara <jancimac@jans-mbp.lan>
2019-03-14 15:02:47 +0000

    full webpack config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
 
 import _ from 'lodash';
 
+// Main Stylesheet
 import './index.less';
+
+// AOS Framework
+import '../node_modules/aos/dist/aos.css';
+import '../node_modules/aos/dist/aos.js';
 
 function component() {
     let element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -9,15 +9,25 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{
-            test: /\.less$/,
-            use: [{
-                loader: 'style-loader' // creates style nodes from JS strings
-            }, {
-                loader: 'css-loader' // translates CSS into CommonJS
-            }, {
-                loader: 'less-loader' // compiles Less to CSS
-            }]
-        }]
+        rules: [
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: 'style-loader' // creates style nodes from JS strings
+                }, {
+                    loader: 'css-loader' // translates CSS into CommonJS
+                }, {
+                    loader: 'less-loader' // compiles Less to CSS
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: 'style-loader' // creates style nodes from JS strings
+                }, {
+                    loader: 'css-loader' // translates CSS into CommonJS
+                }]
+            }
+            ]
     }
 };
